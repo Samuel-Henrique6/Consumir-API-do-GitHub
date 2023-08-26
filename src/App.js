1c18e05eb545ac57447c4c725efb6574faa9667b
samuel-henrique6 <samuellimaarruda@hotmail.com>
2023-08-25 22:37:55 -0400

    update projeto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import './styles/Global.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import NavBar from './components/NavBar';
-import ApiGitHub from './pages/ApiGitHub';
-import { UserContext, UserContextProvider } from './contexts/UserContext'
-import Projetos from './pages/Projetos'
-import { useContext } from 'react';
+import "./styles/Global.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import ApiGitHub from "./pages/ApiGitHub";
+import { UserContext, UserContextProvider } from "./contexts/UserContext";
+import Projetos from "./pages/Projetos";
+import { useContext } from "react";
 
 export function PrivateRoute({ children }) {
-
-  const { logado } = useContext(UserContext)
+  const { logado } = useContext(UserContext);
 
   if (!logado) {
     return <Navigate to="/" replace />;
@@ -17,27 +21,24 @@ export function PrivateRoute({ children }) {
 }
 
 function App() {
-
-  
-
-    return (
-      <UserContextProvider>
-        <Router>
-          <NavBar />
-          <Routes>
-            <Route path='/' element={<ApiGitHub />} />
-            <Route
-              path="/projetos"
-              element={
-                <PrivateRoute>
-                  <Projetos />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
-        </Router>
-      </UserContextProvider>
-    );
-  }
+  return (
+    <UserContextProvider>
+      <Router basename={process.env.PUBLIC_URL}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ApiGitHub />} />
+          <Route
+            path="/projetos"
+            element={
+              <PrivateRoute>
+                <Projetos />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </UserContextProvider>
+  );
+}
 
 export default App;
